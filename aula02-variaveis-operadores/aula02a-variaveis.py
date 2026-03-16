9aab8d5cd6a4dbff909348f762430588fba3618a
M1GU3L0AV1LA <miguelsilverioavila@gmail.com>
2026-03-16 20:45:52 -0300

    aula 02 variaveis

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,22 @@
+print('Olá, mundo!')
+
+print(7+4)
+print("7 + 4")
+print("7" + "4") #CONCATENANDO STRINGS
+
+# VARIÁVEIS
+
+nome = 'Miguel'
+idade = 26
+peso = 71.3
+
+print("Olá", nome, idade, peso)
+print(f"Olá, {nome}!")
+
+# INPUTS --- SIMULAÇÃO DE UM FORMS NO CMD
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+
+print(nome, idade, peso)
