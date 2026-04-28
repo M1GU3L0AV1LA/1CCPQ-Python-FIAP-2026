b6d742ce577499064b9ea8d5443d174581f9d1c4
M1GU3L0AV1LA <miguelsilverioavila@gmail.com>
2026-04-27 21:34:11 -0300

    Aula do dia 27/04/2026

---
+++ Pensamento-Comp- Automação-Py/aula04-while_for/aula04-atividade03.py
@@ -0,0 +1,8 @@
+# Faça um programa que receba a quantidade de produtos que o usuário deseja.
+# A seguir, seu programa deve exibir a mensagem “Produto” a quantidade de vezes que o usuáriosolicitou.
+# Utilize o laço for.
+
+qtd_musicas = int(input("Digite a quantidade de músicas: "))
+
+for i in range(qtd_musicas):
+    print(f"Música {i}")
