b956b64211af7e59791289ceef1f514c1629fa65
M1GU3L0AV1LA <miguelsilverioavila@gmail.com>
2026-08-12 01:04:50 -0300

    Modelagem Lin - 11/08/26

---
+++ Modelagem_Linear/script.py
@@ -0,0 +1,17 @@
+# Preparando o ambiente
+import numpy as np
+from scipy.stats import norm
+
+# Erro Padrão da Média Amostral:
+# ep = 10/np.sqrt(25)
+
+# Probabilidade:
+print(norm.cdf(95, 100, ep))
+
+n= 110/50
+
+# Encontrando o erro padrão da média amostral:
+ep = 0.7/np.sqrt(50)
+
+# Probabilidade:
+print(norm.sf(n,2,ep))
