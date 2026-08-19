f1869b46b9ca4f742e622b45417fe1236acebe80
M1GU3L0AV1LA <miguelsilverioavila@gmail.com>
2026-08-18 21:32:24 -0300

    Aula do dia - 1/08

---
+++ Modelagem_Linear/aula08.py
@@ -0,0 +1,32 @@
+'''# Gerar números inteiros pseudoaleátorios:
+import random
+
+print(random.sample(range(1, 11), 5))
+
+print( [random.randint(1, 100) for _ in range(6)])
+
+# Gerar Números decimais Pseudoaleatórios:
+print( [random.uniform(1, 10) for _ in range(6)])
+
+print( [round(random.uniform(1, 10), 2) for _ in range(6)])
+
+# Gerar números Pseudoaleatórios viciados:
+import random
+
+random.seed(1)
+print( [random.randint(1, 100) for _ in range(6)])'''
+
+# Gerar sorteio de palavras
+import random
+
+nomes = ["Paulo", "Gabriel", "Iara", "Isis", "Lucas", "Zé"]
+print(random.sample(nomes, 3))
+
+# Gerar sorteio de palavras viciadas:
+import numpy as np
+
+nomes = ["Paulo", "Gabriel", "Iara", "Isis", "Lucas", "Zé"]
+vies = [0.10, 0.20, 0.05, 0.15, 0.10, 0.40]
+
+print(np.random.choice(nomes, 5, replace=False, p=vies))
+
